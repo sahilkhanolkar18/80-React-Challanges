2225d477a6ca91435273145f4d288750e47fdb6e
sahil khanolkar <sahil.khanolkar@viraltech.in>
2024-06-23 17:45:35 +0530

    hidden 40 challange

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -745,25 +745,25 @@ const Snippents = [
     ],
     image: Challange39,
   },
-  {
-    id: 40,
-    name: "(pending)Flexible Tooltip",
-    heading: "(pending)Flexible Tooltip",
-    description:
-      "You're given an app that... kind of works. The only problem is when you hover over any of the yellow underlined text, the tooltip renders in the wrong position. Your job is to fix the app so that the tooltip renders correctly - either above the text if there's room, or below it if there's not.To do this, you'll want to utilize the browser's getBoundingClientRect API to get position information about specific elements.You won't need to touch any code other than the Tip component.",
-    problem: [
-      "Position the tooltip above the hovered text if there's enough room",
-      "If there's not enough room above it, and if there's more room below it than above it, position the tooltip below the hovered text",
-      "If there's not enough room above it, and there's less room below it, position the tooltip above the hovered text",
-    ],
-    learningHeading: "(pending)Flexible Tooltip",
-    note: [
-      "Learned passing down props as objects to a Component.",
-      "Destructuring props and using it in component.",
-      "Creating reusable Component.",
-    ],
-    image: Challange1,
-  },
+  // {
+  //   id: 40,
+  //   name: "(pending)Flexible Tooltip",
+  //   heading: "(pending)Flexible Tooltip",
+  //   description:
+  //     "You're given an app that... kind of works. The only problem is when you hover over any of the yellow underlined text, the tooltip renders in the wrong position. Your job is to fix the app so that the tooltip renders correctly - either above the text if there's room, or below it if there's not.To do this, you'll want to utilize the browser's getBoundingClientRect API to get position information about specific elements.You won't need to touch any code other than the Tip component.",
+  //   problem: [
+  //     "Position the tooltip above the hovered text if there's enough room",
+  //     "If there's not enough room above it, and if there's more room below it than above it, position the tooltip below the hovered text",
+  //     "If there's not enough room above it, and there's less room below it, position the tooltip above the hovered text",
+  //   ],
+  //   learningHeading: "(pending)Flexible Tooltip",
+  //   note: [
+  //     "Learned passing down props as objects to a Component.",
+  //     "Destructuring props and using it in component.",
+  //     "Creating reusable Component.",
+  //   ],
+  //   image: Challange1,
+  // },
 ];
 
 export default Snippents;
